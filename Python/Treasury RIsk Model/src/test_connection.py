93d32c782154ce98383a82392a2ee5e5a44ed47a
rowem3 <mr2121@georgetown.edu>
2026-08-19 20:23:31 -0400

    Remove Treasury Risk Model (split into its own repo: treasury-risk-model)

--- Python/Treasury RIsk Model/src/test_connection.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from fredapi import Fred
-
-load_dotenv()
-fred = Fred(api_key=os.getenv("FRED_API_KEY"))
-
-data = fred.get_series('FEDFUNDS')
-print(data.tail())
